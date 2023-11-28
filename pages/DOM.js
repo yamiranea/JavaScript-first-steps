cd9aacbfad1fd668a445445aa4d807fe4ceb9674
yamiranea <yamilarenea@gmail.com>
2023-11-29 00:04:24 +0100

    dom added

--- pages/DOM.js
+++ pages/DOM.js
@@ -8,3 +8,40 @@ document.getElementById("btn").addEventListener("click", function () {
 // 2.
 var newContent = document.getElementById("new-content");
 newContent.innerHTML = "<p>Hello I'm the new content 😀</p>";
+
+// 3.
+var newContent = document.getElementById("new-content");
+var showBtn = document.getElementById("showBtn");
+var hideBtn = document.getElementById("hideBtn");
+
+showBtn.addEventListener("click", function () {
+  newContent.style.display = "block";
+  showBtn.classList.add("hidden");
+  hideBtn.classList.remove("hidden");
+});
+
+hideBtn.addEventListener("click", function () {
+  newContent.style.display = "none";
+  hideBtn.classList.add("hidden");
+  showBtn.classList.remove("hidden");
+});
+
+// 4.
+let names = [
+  "Ana",
+  " Carla",
+  " Sandra",
+  " Marina",
+  " Pedro",
+  " Juan",
+  " Manuel",
+  " Jesus",
+  " Estela",
+  " Carla",
+];
+
+document.getElementById("names-btn").addEventListener("click", function () {
+  var namesMsg = document.getElementById("names-msg");
+  namesMsg.style.display = "block";
+  namesMsg.innerHTML = names;
+});
